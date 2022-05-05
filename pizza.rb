2df727618a58230b93da8ea8ab4eebb99f9d97a5
Mike Madsen <mikewmadsen@gmail.com>
2022-05-05 12:07:59 -0700

    Add new york style

--- pizza.rb
+++ pizza.rb
@@ -1,8 +1,14 @@
+require_relative "toppings/pepperoni"
+
 class Pizza
 
   def initialize(toppings, cooking_time)
     @toppings = toppings
     @cooking_time = cooking_time
+  end
+
+  def newyorker
+    puts "ayy im walkin ere"
   end
   
   
